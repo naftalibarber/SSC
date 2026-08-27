f24d22f336f325ed849cd4383fab00d43e765125
naftalibarber <naftalibarber@gmail.com>
2026-08-27 11:53:42 +0300

    Route validated 4x4 preview through SSCPreviewV1

--- code/js/preview/ssc-preview-v1-integration.js
+++ code/js/preview/ssc-preview-v1-integration.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const V1_EVENTS=new Set(['222','333']);
+  const V1_EVENTS=new Set(['222','333','444']);
   const legacyPreview=window.SSCCubePreview||null;
   const legacyRender=legacyPreview?.render?.bind(legacyPreview)||null;
   const managerRender=window.SSCPreviewManager?.render?.bind(window.SSCPreviewManager)||null;
